0f01068e558ae1f44aca03196b9d9ab8e207e9ff
Natassa Farmaki <natazf@gmail.com>
2023-10-14 16:31:20 +0300

    add v-model mappings

--- src/views/CustomerView.vue
+++ src/views/CustomerView.vue
@@ -1,74 +1,85 @@
 <template>
-    <div>
+    <div class="event-form">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Event Planning Request Form</title>
+        </head>
         <body>
             <div>
                 <form>
-                    <div>
-                        <label for="recordNumber">Record Number:</label>
+                    <div class="form-group ">
+                        <label >Record Number:</label>
                         <div>
-                            <input type="text" id="recordNumber" name="recordNumber">
+                            <input v-model="recordNumber"  type="text" id="recordNumber" name="recordNumber">
                         </div>
                     </div>
                     <div>
                         <label for="clientName">Client Name:</label>
                         <div>
-                            <input type="text" name="clientName">
+                            <input v-model="name" type="text" name="clientName">
                         </div>
                     </div>
                     <div>
                         <label for="eventType">Event Type:</label>
                         <div>
-                            <input type="text" name="eventType">
+                            <input v-model="eventType" type="text" name="eventType">
                         </div>
                     </div>
                     <div>
                         <label for="dateFrom">From:</label>
                         <div>
-                            <input type="date" id="dateFrom" name="dateFrom">
+                            <input v-model="dateFrom" type="date" id="dateFrom" name="dateFrom">
                         </div>
                         <label for="dateTo">To:</label>
                         <div>
-                            <input type="date" id="dateTo" name="dateTo">
+                            <input v-model="dateTo" type="date" id="dateTo" name="dateTo">
                         </div>
                     </div>
                     <div>
                         <label for="attendees">Expected Number of Attendees:</label>
                         <div>
-                            <input type="number" id="attendees" name="attendees">
+                            <input v-model="numAttendees" type="number" id="attendees" name="attendees">
                         </div>
                     </div>
                     <div>
                         <label for="preferences">Preferences:</label>
                         <div>
                             <div>
-                                <input type="checkbox" id="decorations" name="preferences">
+                                <input type="checkbox" id="decorations" value="decorations" v-model="selectedPreferences">
                                 <label for="decorations">Decorations</label>
                             </div>
                             <div>
-                                <input type="checkbox" id="parties" name="preferences">
+                                <input type="checkbox" id="parties" value="parties" v-model="selectedPreferences">
                                 <label for="parties">Parties</label>
                             </div>
                             <div>
-                                <input type="checkbox" id="photosFilming" name="preferences">
+                                <input type="checkbox" id="photosFilming" value="photosFilming"  v-model="selectedPreferences">
                                 <label for="photosFilming">Photos/Filming</label>
                             </div>
                             <div>
-                                <input type="checkbox" id="breakfast" name="preferences">
+                                <input type="checkbox" id="breakfast" value="breakfast" v-model="selectedPreferences" >
                                 <label for="breakfast">Breakfast</label>
                             </div>
                             <div>
-                                <input type="checkbox" id="lunch" name="preferences">
+                                <input type="checkbox" id="lunch" value="lunch" v-model="selectedPreferences">
                                 <label for="lunch">Lunch</label>
                             </div>
                             <div>
-                                <input type="checkbox" id="dinner" name="preferences">
+                                <input type="checkbox" id="dinner" value="dinner" v-model="selectedPreferences">
                                 <label for="dinner">Dinner</label>
                             </div>
                             <div>
-                                <input type="checkbox" id="softDrinks" name="preferences">
+                                <input type="checkbox" id="softDrinks" value="softDrinks" v-model="selectedPreferences">
                                 <label for="softDrinks">Soft/Drinks</label>
                             </div>
                         </div>
+                    </div>
+                    <div v-if="selectedPreferences.length!=0">
+                        Selected Preferences:
+                            <ul>
+                                <li v-for="selectedPreference in selectedPreferences" :key="selectedPreference">{{ selectedPreference }}</li>
+                            </ul>
                     </div>
                     <div>
                         <label for="expectedBudget">Expected Budget:</label>
@@ -78,7 +89,7 @@
                     </div>
                     <div>
                         <div>
-                            <button type="submit">Submit</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
                     </div>
                 </form>
@@ -91,9 +102,60 @@
 
 export default {
     name: "CustomerView",
-
+    data:function () {
+        return {
+            recordNumber: "",
+            name: "",
+            eventType: "",
+            dateFrom: "",
+            dateTo: "",
+            numAttendees:null,
+            selectedPreferences:[],
+            expectedBudget:null
+        }
+    },
+    method:{
+    }
 }
 
 </script>
 
-<style></style>
+<style scoped>
+.event-form {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.label {
+  font-weight: bold;
+}
+
+.input {
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+.btn {
+  background-color: #007BFF;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #0056b3;
+}
+</style>
